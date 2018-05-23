8b8fd10ec467e38ba9907247247a9b886d1f69b0
Leo <leo.roy@soriade.fr>
2018-05-23 15:47:00 +0200

    Details Stagiaire + Inscription/Modif Stagiaire v2 (verifs des données saisies OK + layout upgrade)

--- app/src/main/java/com/ldnr/evenements/DetailsStagiaireActivity.java
+++ app/src/main/java/com/ldnr/evenements/DetailsStagiaireActivity.java
@@ -12,20 +12,20 @@ import com.squareup.picasso.Picasso;
  */
 public class DetailsStagiaireActivity extends AppCompatActivity {
 
-    // Ici les clés des extras contenant les informations
-    private String EXTRA_IMAGE = "imageUrl";
-    private String EXTRA_NOM = "nom";
-    private String EXTRA_PRENOM = "prenom";
-    private String EXTRA_AGE = "age";
-    private String EXTRA_MAIL = "mail";
-    private String EXTRA_TEL = "tel";
-    private String EXTRA_SESSION = "session";
-    private String EXTRA_FORMATION = "formation";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details_stagiaire);
+
+        // Ici les clés des extras contenant les informations
+        String EXTRA_IMAGE = "imageUrl";
+        String EXTRA_NOM = "nom";
+        String EXTRA_PRENOM = "prenom";
+        String EXTRA_AGE = "age";
+        String EXTRA_MAIL = "mail";
+        String EXTRA_TEL = "tel";
+        String EXTRA_SESSION = "session";
+        String EXTRA_FORMATION = "formation";
 
         // Récupération des extras
         Bundle extras = getIntent().getExtras();
